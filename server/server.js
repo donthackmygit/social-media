fdd9183fda84cb93f7fe676daeb320b23d56b432
lmh05k68 <hle290105@gmail.com>
2025-08-29 11:53:05 +0700

    fix server.js 29/8/25

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/',(req, res) => res.send('Server is running'))
-app.use('/api/inggest',serve({ client: inngest, functions }))
+app.use('/api/inngest',serve({ client: inngest, functions }))
 
 const PORT = process.env.PORT || 4000;
 
